9fee4d2072751eebef1d3a54a71e1a9bd78c1667
Ali Eren Kayhan <a.erenkayhan@gmail.com>
2024-08-30 17:26:40 +0300

    Addin react native maps

---
+++ uber-clone/components/Map.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps';
+import tw from 'twrnc'
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
+
+const Map = () =>
+{
+    const origin = useSelector(selectOrigin);
+
+    return (
+        <MapView
+            style={tw`flex-1`}
+            mapType='mutedStandard'
+            initialRegion={{
+                latitude: origin.location.lat,
+                longitude: origin.location.lng,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            }}
+        />
+    )
+}
+
+export default Map
+
+const styles = StyleSheet.create({})
